ee6a74d7a3ca23f13243b8586840f9256084d31b
anait31 <anaitullah198@gmail.com>
2025-02-09 11:35:50 +0800

    Update design

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,13 @@ import 'react-tabs/style/react-tabs.css';
 import { IoLogoWhatsapp } from "react-icons/io";
 import { RiWechat2Fill } from "react-icons/ri";
 import { SiLivechat } from "react-icons/si";
+import heroImg from '../src/assets/1.png'
 
 function App() {
 
   return (
     <>
-      <div className=''>
+      <div className='bg-black text-white'>
         <div className='bg-black'>
           <div className="navbar text-neutral-content max-w-6xl mx-auto">
             <Link className='' to={'/'}>
@@ -20,29 +21,32 @@ function App() {
             </Link>
           </div>
         </div>
-        <section className='py-16 bg-slate-300'>
-          <div id="hero" className='max-w-6xl mx-auto text-center'>
-            <h1 className='text-5xl'>Stay Accessible Anytime, Anywhere</h1>
-            <p className='text-2xl my-2'>Please save this info domain for backup purposes.</p>
-            <p className=''><span className='block text-xl font-semibold'>Dear Valued Members,</span> Should you feel slowness or difficulties to access our website, kindly visit the alternate link for better performance.</p>
-            <div className=''>
-              <p>Select one of the following link to continue:</p>
-              <div className='space-x-3 mt-4'>
-                <Link to={'/'}><button className='bg-orange-400 px-6 py-2 rounded-md'>Link 1</button></Link>
-                <Link to={'/'}><button className='bg-orange-400 px-6 py-2 rounded-md'>Link 2</button></Link>
-                <Link to={'/'}><button className='bg-orange-400 px-6 py-2 rounded-md'>Link 3</button></Link>
+        <section className='pb-16 pt-8 bg-black text-white'>
+          <div className='flex items-center max-w-6xl mx-auto '>
+            <div id="hero" className='text-center w-1/2'>
+              <h1 className='text-5xl'>Stay Accessible Anytime, Anywhere</h1>
+              <p className='text-2xl my-2'>Please save this info domain for backup purposes.</p>
+              <p className=''><span className='text-xl font-semibold'>Dear Valued Members,</span> Should you feel slowness or difficulties to access our website, kindly visit the alternate link for better performance.</p>
+              <div className=''>
+                <p>Select one of the following link to continue:</p>
+                <div className='space-x-3 mt-4'>
+                  <Link to={'https://hpwin88.com'}><button className='bg-orange-400 px-6 py-2 rounded-md'>Link 1</button></Link>
+                  <Link to={'https://hpwinvipgo.com'}><button className='bg-orange-400 px-6 py-2 rounded-md'>Link 2</button></Link>
+                  <Link to={'https://hpwinvipnow.com'}><button className='bg-orange-400 px-6 py-2 rounded-md'>Link 3</button></Link>
+                </div>
               </div>
             </div>
+            <img className='w-1/2' src={heroImg} alt="" />
           </div>
         </section>
-        <section className='bg-slate-300 pb-16'>
+        <section className='py-16'>
           <div className='max-w-6xl mx-auto'>
             <div id="hero" className='text-center'>
               <h1 className='text-4xl'>Having Difficulties Accessing Our Site?</h1>
               <p className='text-2xl my-2'>Please Contact With Our Special Customer Support Specialist.</p>
-              <div className=''>
+              <div className='mt-8'>
                 <div className='space-x-3 flex justify-center'>
-                  <Link className='border-2 p-4 rounded-lg' to={'/'}>
+                  <Link className='border-2 p-4 rounded-lg' to={'https://api.whatsapp.com/send?phone=60182536271'}>
                     <IoLogoWhatsapp className='text-4xl w-full'></IoLogoWhatsapp>
                     <button className='px-6 py-2 rounded-md'>Whatsapp</button>
                   </Link>
@@ -70,48 +74,48 @@ function App() {
                 </div>
                 <TabPanel className="mt-6">
                   <ol className='list-decimal'>
-                    <li>Click on the Start menu Settings (⚙️ icon).</li>
-                    <li>Click on Network & Internet.</li>
-                    <li>Click on Change adapter options.</li>
+                    <li>Click on the <span className='font-bold text-orange-500'>Start menu </span> {'>'} <span className='font-bold text-orange-500'>Settings</span> (⚙️ icon).</li>
+                    <li>Click on <span className='font-bold text-orange-500'>Network & Internet</span>.</li>
+                    <li>Click on <span className='font-bold text-orange-500'>Change adapter options</span>.</li>
                     <li>Right click on the Wi-Fi network you are connected to.</li>
+                    <li>Click <span className='font-bold text-orange-500'>Properties</span>.</li>
+                    <li>Select Internet <span className='font-bold text-orange-500'> Protocol Version 4</span>.</li>
                     <li>Click Properties.</li>
-                    <li>Select Internet Protocol Version 4.</li>
-                    <li>Click Properties.</li>
-                    <li>Click Use the following DNS server address.</li>
+                    <li>Click <span className='font-bold text-orange-500'>Use the following DNS server address</span>:</li>
                     <li>
                       Replace any IP addresses that may be already listed, with:
-                      <div>
+                      <div className='bg-slate-800 p-6 rounded-lg my-2'>
                         <ol>
                           <li>Cloudflare</li>
                           <li>1.1.1.1</li>
                           <li>1.0.0.1</li>
-                          <li>Or</li>
+                          <li className='my-2'>OR</li>
                           <li>Google Public DNS</li>
                           <li>8.8.8.8</li>
                           <li>8.8.4.4</li>
                         </ol>
                       </div>
                     </li>
-                    <li>Click OK.</li>
-                    <li>Now go to Internet Protocol Version 6.</li>
-                    <li>Click Properties.</li>
-                    <li>Click Use the following DNS server address.</li>
+                    <li>Click <span className='font-bold text-orange-500'>OK</span>.</li>
+                    <li>Now go to <span className='font-bold text-orange-500'>Internet Protocol Version 6</span>.</li>
+                    <li>Click <span className='font-bold text-orange-500'>Properties</span>.</li>
+                    <li>Click <span className='font-bold text-orange-500'>Use the following DNS server address</span>:</li>
                     <li>
                       Remove any IP addresses that may be already listed and in their place add:
-                      <div>
+                      <div className='bg-slate-800 p-6 rounded-lg my-2'>
                         <ol>
                           <li>Cloudflare</li>
                           <li>2606:4700:4700::1111</li>
                           <li>2606:4700:4700::1001</li>
-                          <li>Or</li>
+                          <li className='my-2'>OR</li>
                           <li>Google Public DNS</li>
                           <li>2001:4860:4860::8888</li>
                           <li>2001:4860:4860::8844</li>
                         </ol>
                       </div>
                     </li>
-                    <li>Click OK.</li>
-                    <li>Click Close.</li>
+                    <li>Click <span className='font-bold text-orange-500'>OK</span>.</li>
+                    <li>Click <span className='font-bold text-orange-500'>Close</span>.</li>
                   </ol>
                 </TabPanel>
                 <TabPanel>
@@ -265,10 +269,10 @@ function App() {
                 </TabPanel>
               </Tabs>
             </div>
-            <div>
+            <div className='bg-slate-800 p-6 rounded-lg my-2 border-l-2'>
               <p><span>Note:</span> Setting up a static IP address to configure a DNS server may prevent you from connecting to some public Wi-Fi networks that use captive portals (these are the web pages some wireless networks employ to let users log in and use their services).</p>
-              <p>If you are experiencing connectivity issues related to captive portals:</p>
-              <ol className='list-decimal'>
+              <p className='my-2'>If you are experiencing connectivity issues related to captive portals:</p>
+              <ol className='list-decimal ml-8'>
                 <li>Remove the static IP addresses from the device.</li>
                 <li>Connect to the Wi-Fi network.</li>
                 <li>Once the connection has been established, re-add the static IP addresses.</li>
@@ -279,21 +283,10 @@ function App() {
         <div className='bg-neutral'>
           <footer className="footer text-neutral-content items-center p-4 max-w-6xl mx-auto">
             <aside className="grid-flow-col items-center">
-              <svg
-                width="36"
-                height="36"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                fillRule="evenodd"
-                clipRule="evenodd"
-                className="fill-current">
-                <path
-                  d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
-              </svg>
-              <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+              <p>© {new Date().getFullYear()} HPWIN - All right reserved</p>
             </aside>
             <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-              <a>
+              <Link to={'/'}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -303,8 +296,8 @@ function App() {
                   <path
                     d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
                 </svg>
-              </a>
-              <a>
+              </Link>
+              <Link to={'/'}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -314,8 +307,8 @@ function App() {
                   <path
                     d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
                 </svg>
-              </a>
-              <a>
+              </Link>
+              <Link to={'/'}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -325,7 +318,7 @@ function App() {
                   <path
                     d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
                 </svg>
-              </a>
+              </Link>
             </nav>
           </footer>
         </div>
